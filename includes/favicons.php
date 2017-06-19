fad346c805562cba7bbae85a43882cd3907a30dc
ASDAFF <asdaff.asad@yandex.ru>
2017-06-20 02:16:02 +0500

    Update

--- includes/favicons.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ASDAFF
- * Date: 19.06.2017
- * Time: 20:05
- */
--- includes/pages/main.php
+++
@@ -1,6 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Главная страница");
-?>
-<h1>Главная страница</h1>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>

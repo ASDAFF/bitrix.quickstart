e99260e9221fbb2f756fd1e82fe89ffedb3d10e0
ASDAFF <asdaff.asad@yandex.ru>
2017-10-06 13:37:08 +0500

    Добавил компонент catalog

--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -1,24 +1,55 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<!DOCTYPE html>
-<html lang="<?=LANG;?>" xmlns="http://www.w3.org/1999/xhtml">
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/**
+ * @var CUser $USER
+ * @var CMain $APPLICATION
+ * @var $full_width - переменная в которой будет true если выполнится условие из  $arFullWidthPages
+ */
+?><!DOCTYPE html>
+<html lang="<?= LANGUAGE_ID ?>">
 <head>
-	<title><?$APPLICATION->ShowTitle();?></title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="<?= SITE_CHARSET ?>">
+    <title><? $APPLICATION->ShowTitle() ?></title>
+    <?
+    //Тут мета-теги
+    $APPLICATION->ShowMeta("robots", false, false);
+    $APPLICATION->ShowMeta("keywords", false, false);
+    $APPLICATION->ShowMeta("description", false, false);
+    ?>
+    <meta name="yandex-verification" content="xxxxxxxxxxxxxxxx">
+    <meta name="google-site-verification" content="xxxxxxxxxxxxxxxx">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <?
+    //Тут канонический url
+    $APPLICATION->ShowLink("canonical", null, false);
+
+    //Тут стили шаблона сайта
+    $APPLICATION->SetAdditionalCSS(SITE_TEMPLATE_PATH . '/css/style.css');
+
+    //Тут выводим стили
+    $APPLICATION->ShowCSS(true, false);
+    ?>
+    <!--[if lt IE 9]>
+    <script type="text/javascript"
+            src="<?=CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH.'/js/ie8-polyfill.js');?>"></script>
 
 
-	<!--
-		# Require jQuery from CDN
-		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
-	-->
+    <![endif]-->
+    <script type="text/javascript"
+            src="<?= CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH . '/js/jquery-1.11.2_min.js'); ?>"></script>
+    <?
+    //Это встроенная в ядро Битрикс jQuery, если подключать ее, то строку подключения jQuery 1.11.2 выше надо удалить.
+    //CJSCore::Init(array('jquery'));
 
-	<!-- 
-		# Example of including javascript libraries or plugins 
-		<script type="text/javascript" src="<?=SITE_TEMPLATE_PATH;?>/js/plugin-name/plugin.js">
-		<link rel="stylesheet" type="text/css" href="<?=SITE_TEMPLATE_PATH;?>/js/plugin-name/plugin.css" media="screen"/>
-	-->
+    //Тут скрипты
+    $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/uikit/core.min.js');
+    $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/uikit/grid.min.js');
 
-	<?$APPLICATION->ShowHead();?>
+    //Тут выводим скрипты
+    $APPLICATION->ShowHeadStrings();
+    $APPLICATION->ShowHeadScripts();
+    ?>
 </head>
 <body>
-<?$APPLICATION->ShowPanel();?>
+<div id="panel"><? $APPLICATION->ShowPanel(); ?></div>

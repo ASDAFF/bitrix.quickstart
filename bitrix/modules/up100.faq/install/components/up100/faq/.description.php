56828a6787c74c7d9dc05b60e33a9e9cabf21971
ASDAFF <asdaff.asad@yandex.ru>
2018-01-18 21:06:33 +0500

    Добавлен модуль "up100.faq"

---
+++ bitrix/modules/up100.faq/install/components/up100/faq/.description.php
@@ -0,0 +1,15 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+	"NAME" => '100up FAQ',
+	"ICON" => "/images/icon.gif",
+	"COMPLEX" => "N",
+	"PATH" => array(
+		"ID" => "utility",
+		"CHILD" => array(
+			"ID" => "up100",
+			"NAME" => '100up',
+			"SORT" => 30
+		),
+	),
+);
+?>
---
+++ bitrix/modules/up100.faq/lang/ru/install/index.php
@@ -0,0 +1,6 @@
+<?
+$MESS["up100.faq_MODULE_NAME"] = "Частые вопросы";
+$MESS["up100.faq_MODULE_DESC"] = "";
+$MESS["up100.faq_PARTNER_NAME"] = "Интернет студия 100up";
+$MESS["up100.faq_PARTNER_URI"] = "http://www.100up.ru";
+?>

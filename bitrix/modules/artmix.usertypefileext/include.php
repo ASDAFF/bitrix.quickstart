39aa33c301bcec60e468a025aac2156cc3c82c9f
ASDAFF <asdaff.asad@yandex.ru>
2018-01-18 22:30:10 +0500

    Добавлен модуль "Пользовательское свойство файл (с поддержкой drag & drop)"

---
+++ bitrix/modules/artmix.usertypefileext/include.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Created by Artmix.
+ * User: Oleg Maksimenko <oleg.39style@gmail.com>
+ * Date: 02.06.2016
+ */
---
+++ bitrix/modules/artmix.usertypefileext/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "2016-06-03 11:08:40"
+);
+?>

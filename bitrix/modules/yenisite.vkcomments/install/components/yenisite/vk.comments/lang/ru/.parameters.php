e513ab7073c34bcc7fef488567777abea0258411
ASDAFF <asdaff.asad@yandex.ru>
2018-01-18 17:49:04 +0500

    Добавлен модуль "Комментарии ВКонтакте"

---
+++ bitrix/modules/yenisite.vkcomments/install/components/yenisite/vk.comments/lang/ru/.parameters.php
@@ -0,0 +1,15 @@
+<?
+	$MESS["COM_WIDTH_NAME"] = "Ширина";
+	$MESS["COM_HEIGHT_NAME"] = "Высота";
+	$MESS["COM_AMMOUNT_NAME"] = "Количество комменатриев";
+	$MESS["API_ID_NAME"] = "API ID";
+	$MESS["AUTO_PUBLISH_NAME"] = "Автоматическая публикация в статусе пользователя";
+	$MESS["NO_REAL_TIME_NAME"] = "Обновление комментариев в реальном времени";
+	$MESS["ATTACMENTS_NAME_GROUP"] = "Настройка типов файлов для прикрепления к комментарию";
+	$MESS["ATTACH_ALL_NAME"] = "Прикреплять всё";
+	$MESS["ATTACH_GRAFFITI_NAME"] = "Прикреплять граффити";
+	$MESS["ATTACH_PHOTO_NAME"] = "Прикреплять фотографии";
+	$MESS["ATTACH_AUDIO_NAME"] = "Прикреплять аудиозаписи";
+	$MESS["ATTACH_VIDEO_NAME"] = "Прикреплять видеозаписи";
+	$MESS["ATTACH_LINK_NAME"] = "Прикреплять ссылки";
+?>

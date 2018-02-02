8d4fdf969240bf1053353fbb30b6638a4b485ff2
ASDAFF <asdaff.asad@yandex.ru>
2018-02-02 14:35:12 +0500

    Добавлен модуль "Простой кеширующий компонент"

---
+++ bitrix/modules/tega.simplecache/install/components/tega/simplecache/templates/.default/template.php
@@ -0,0 +1,6 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$this->setFrameMode(true);	
+?>
+
+<pre><?print_r($arResult);?></pre>
---
+++ bitrix/modules/tega.simplecache/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.1",
+	"VERSION_DATE" => "2016-05-05 18:19:56"
+);
+?>

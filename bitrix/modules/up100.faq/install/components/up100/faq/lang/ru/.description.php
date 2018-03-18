67876b0acc4957fb7bfd55480a3d653ac0ca33f2
ASDAFF <asdaff.asad@yandex.ru>
2018-03-18 19:37:11 +0500

    Обновление

--- bitrix/modules/up100.faq/install/components/up100/faq/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['T_IBLOCK_DESC_LIST'] = "Список новостей";
-$MESS ['T_IBLOCK_DESC_LIST_DESC'] = "Список новостей из одного информационного блока.";
-$MESS ['T_IBLOCK_DESC_NEWS'] = "Статьи и новости";
-?>
--- bitrix/modules/up100.faq/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.0.3",
-	"VERSION_DATE" => "2012-11-21 11:57:59"
-);
-?>

0dfa38c8237b9e74605e10ff2f3d0c15915a945d
ASDAFF <asdaff.asad@yandex.ru>
2018-10-16 14:39:27 +0500

    Контакты в  микроформате hCard

--- bitrix/modules/epir.hcard/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "0.0.1",
-	"VERSION_DATE" => "2011-12-19 16:13:00"
-);
-?>
--- local/components/seo/hcard/.description.php
+++ local/components/seo/hcard/.description.php
@@ -8,7 +8,7 @@ $arComponentDescription = array(
 	"SORT" => 1,
 	"ICON" => "/images/hCard.gif",
     	'PATH' => array(
-            'ID' => 'epir',//'event-list',
+            'ID' => 'custom',
             "NAME" => GetMessage("EPIR_IBLOCK_DESC"),//'event-list',
         ),
 

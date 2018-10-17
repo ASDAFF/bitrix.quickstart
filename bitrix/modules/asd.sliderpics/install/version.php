fcf39036f319ee7444c705fdb14110568b48236e
ASDAFF <asdaff.asad@yandex.ru>
2018-10-17 09:58:38 +0500

    Слайдер картинок

--- bitrix/modules/asd.sliderpics/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.1.2",
-	"VERSION_DATE" => "2014-06-11 10:23:40"
-);
-?>
--- bitrix/modules/asd.sliderpics/lang/en/install/install.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['ASD_MODULE_NAME'] = "Слайдер картинок";
-$MESS ['ASD_MODULE_DESCRIPTION'] = "Компонент выводит картинки, плавно сменяющие друг друга.";
-$MESS ['ASD_PARTNER_NAME'] = "Долганин Антон";
-?>

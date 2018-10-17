16da57d847738321de0671e5a9da6f4452685b24
ASDAFF <asdaff.asad@yandex.ru>
2018-10-17 12:40:15 +0500

    Удаление модуля "Увеличительное стекло RetinaZoom"

--- bitrix/modules/mdsoft.retinazoom/install/components/mdsoft/retinazoom/lang/ru/.description.php
+++
@@ -1,7 +0,0 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
-
-<?
-$MESS ['MDSOFT_RETINA_NAME'] = "Ретина зуммер";
-$MESS ['MDSOFT_RETINA_DESCR'] = "Зуммер для картинок как на iPad";
-$MESS ['MDSOFT_RETINA_PATH_ID'] = "MDSoft";
-?>
--- bitrix/modules/mdsoft.retinazoom/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "0.1.2",
-	"VERSION_DATE" => "2014-05-22 11:49:00"
-);
-?>

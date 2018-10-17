ce32d30d19273ff743ce4a950be56c707fc494e7
ASDAFF <asdaff.asad@yandex.ru>
2018-10-17 11:26:58 +0500

    Fotorama (Фоторама для Битрикс)

--- bitrix/modules/htc.fotoramabx/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.3.2",
-	"VERSION_DATE" => "2014-05-04 12:12:10"
-);
-?>
--- bitrix/modules/htc.fotoramabx/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["htc.fotoramabx_MODULE_NAME"] = "Fotorama (Фоторама для Битрикс)";
-$MESS["htc.fotoramabx_MODULE_DESC"] = "";
-$MESS["htc.fotoramabx_PARTNER_NAME"] = "Центр Высоких Технологий";
-$MESS["htc.fotoramabx_PARTNER_URI"] = "http://htc-cs.ru";
-?>

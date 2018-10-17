eee9fa8205d4ba0323688eecac813971ef6ea3e6
ASDAFF <asdaff.asad@yandex.ru>
2018-10-17 11:15:02 +0500

    Слайдер работ с превью-баром

--- bitrix/modules/gtech.sectionsliding/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.2.0",
-	"VERSION_DATE" => "2010-12-10 00:00:00"
-);
-?>
--- bitrix/modules/gtech.sectionsliding/lang/en/install/install.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['ASD_MODULE_NAME'] = "Слайдер картинок инфоблока";
-$MESS ['ASD_MODULE_DESCRIPTION'] = "Слайдер картинок с превьюв-баром";
-$MESS ['ASD_PARTNER_NAME'] = "Веб-студия G-tech";
-?>

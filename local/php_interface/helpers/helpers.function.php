42ad3c1ec633963b675462c38764ec84b9424e88
ASDAFF <asdaff.asad@yandex.ru>
2019-02-02 05:35:09 +0500

    получить родительские разделы с пользовательскими свойствами

--- local/php_interface/helpers/helpers.function.php
+++ local/php_interface/helpers/helpers.function.php
@@ -943,3 +943,54 @@ function cp_month_name($month, $format)
     $timestamp = mktime(1,1,1, $month, 1, date('Y'));
     return date($format, $timestamp);
 }
+
+// получить родительские разделы с пользовательскими свойствами
+function getStructureUserProps($id, $section=false, $arUf=array()) {
+    $id = (int)$id;
+    if (!$id) { return false; }
+    if ($section) {
+        $res = CIBlockSection::GetByID($id);
+        if ( $ar = $res->GetNext() ) {
+            $res = CIBlockSection::GetList(array(), array('IBLOCK_ID'=>$ar['IBLOCK_ID'], 'ID'=>$ar['ID']), false, $arUf);
+        } else { return false; }
+    } else {
+        $res = CIBlockElement::GetByID($id);
+        if ( $ar = $res->GetNext() ) {
+            $res = CIBlockSection::GetList(array(), array('IBLOCK_ID'=>$ar['IBLOCK_ID'], 'ID'=>$ar['IBLOCK_SECTION_ID']), false, $arUf);
+        } else { return false; }
+    }
+    // херню подсунули
+    if ( !$res->SelectedRowsCount() ) { return false; }
+    $arSect = $res->GetNext();
+    $arStructure = array();
+    $res = CIBlockSection::GetList(array('LEFT_MARGIN'=>'ASC'), array(
+        'IBLOCK_ID' => $arSect['IBLOCK_ID'],
+        '<=LEFT_BORDER' => $arSect['LEFT_MARGIN'],
+        '>=RIGHT_BORDER' => $arSect['RIGHT_MARGIN'],
+        '<DEPTH_LEVEL' => $arSect['DEPTH_LEVEL'],
+    ), false, $arUf);
+
+    while($ar = $res->GetNext()){
+        $arStructure[] = $ar;
+    }
+    $arStructure[] = $arSect;
+    return $arStructure;
+}
+
+// получить родительские разделы без пользовательских свойств
+function getStructure($id, $section=false) {
+    $id = (int)$id;
+    if (!$id) { return false; }
+    if (!$section) {
+        $res = CIBlockElement::GetByID($id);
+        if ( $ar = $res->GetNext() ){
+            $id = $ar['IBLOCK_SECTION_ID'];
+        } else { return false; }
+    }
+    $res = CIBlockSection::GetNavChain(false, $id);
+    $arStructure = array();
+    while($ar = $res->GetNext()){
+        $arStructure[] = $ar;
+    }
+    return $arStructure;
+}

491a1c09c977ae95599787eede00199d638e87c2
ASDAFF <asdaff.asad@yandex.ru>
2019-06-06 13:11:59 +0500

    JS и CSS для административного интерфейса

--- local/php_interface/admin_header.php
+++ local/php_interface/admin_header.php
@@ -8,11 +8,11 @@
  * Header административной части
  */
 ?>
-// CSS
+
 <link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/aat.iblockprops/interface.css">
 <link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/global.css">
 
-// JS
+
 <script type="text/javascript" src="/local/codenails/backend/js/jquery.min.js"></script>
 
 <script type="text/javascript" src="/local/codenails/backend/js/aat.iblockprops/interface.js"></script>

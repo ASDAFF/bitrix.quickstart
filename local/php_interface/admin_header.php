2fedddd8a9d37f3a121aa51145b42db28783c16a
ASDAFF <asdaff.asad@yandex.ru>
2019-06-17 19:09:31 +0500

    fixed

--- local/php_interface/admin_header.php
+++ local/php_interface/admin_header.php
@@ -9,11 +9,11 @@
  */
 ?>
 
-<link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/aat.iblockprops/interface.css">
-<link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/global.css">
+<link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/aat.iblockprops/interface.min.css">
+<link rel="stylesheet" type="text/css" href="/local/codenails/backend/css/global.min.css">
 
 
 <script type="text/javascript" src="/local/codenails/backend/js/jquery.min.js"></script>
 
-<script type="text/javascript" src="/local/codenails/backend/js/aat.iblockprops/interface.js"></script>
-<script type="text/javascript" src="/local/codenails/backend/js/global.js"></script>
+<script type="text/javascript" src="/local/codenails/backend/js/aat.iblockprops/interface.min.js"></script>
+<script type="text/javascript" src="/local/codenails/backend/js/global.min.js"></script>

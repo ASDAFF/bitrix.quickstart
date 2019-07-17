8ae4d0cad24af8189e846b176a167a6f6a7061ce
ASDAFF <asdaff.asad@yandex.ru>
2019-07-17 11:52:38 +0500

    up

--- for_install/modules_recommended/tsv.nfpp/lang/ru/install/index.php
+++
@@ -1,8 +0,0 @@
-<?
-$MESS["TSV_NFPP_MODULE_NAME"] = "Распечатка массива на экран в виде дерева";
-$MESS["TSV_NFPP_MODULE_DESCRIPTION"] = "Модуль позволяет распечатывать массивы на экран в красивом древовидном стиле.";
-$MESS["TSV_NFPP_PARTNER_NAME"] = "Талызенков Сергей";
-$MESS["TSV_NFPP_NO_RIGHTS"] = "Нет прав на установку.";
-$MESS["TSV_NFPP_NO_RIGHTS_UNINSTALL"] = "Нет прав на удаление.";
-
-?>

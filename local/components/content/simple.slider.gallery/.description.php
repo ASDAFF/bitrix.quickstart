2526f5d74838bee729d7b1201c4a3a03fc07f376
ASDAFF <asdaff.asad@yandex.ru>
2019-07-17 16:34:39 +0500

    up

--- local/components/content/simple.slider.gallery/.description.php
+++ local/components/content/simple.slider.gallery/.description.php
@@ -4,10 +4,35 @@ $arComponentDescription = array(
  "NAME" =>GetMessage("SS_SSG_NAME"),
  "DESCRIPTION" => GetMessage("SS_SSG_DESCRIPTION"),
  "PATH" => array(
- 	"ID" => "asdaff",
+ 	"ID" => "ASDAFF",
+     "SORT" => 10,
 	"NAME" => GetMessage("SS_NAME"),
-	"SORT" => 10,
+             "CHILD" => array(
+            "ID" => 'Контент',
+            "NAME" => '',
+            "SORT" => 500,
+        )
  ),
  "ICON" => "/images/icon.gif",
 );
+
+
+
+
+//$arComponentDescription = array(
+//    "NAME" => GetMessage("SORT_PANEL_COMPONENT_NAME_VALUE"),
+//    "DESCRIPTION" => GetMessage("SORT_PANEL_COMPONENT_DESCRIPTION_VALUE"),
+//    "ICON" => "/images/icon.gif",
+//    "SORT" => 100,
+//    "PATH" => array(
+//        "ID" => "ASDAFF",
+//        "SORT" => 500,
+//        "NAME" => GetMessage("SORT.PANEL_COMPONENTS_FOLDER_NAME"),
+//        "CHILD" => array(
+//            "ID" => GetMessage("SORT_PANEL_COMPONENT_TYPE_CONTENT_VALUE"),
+//            "NAME" => '',
+//            "SORT" => 500,
+//        )
+//    ),
+//);
 ?>

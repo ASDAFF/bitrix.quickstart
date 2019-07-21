29cf0c8e4f115feeed33b334226a053bf87ac3b9
ASDAFF <asdaff.asad@yandex.ru>
2019-07-22 04:10:20 +0500

    up

--- for_install/modules_recommended/simai.complexprop/lang/ru/install/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $MESS;
-
-$MESS['SMCP_MODULE_NAME'] = 'SIMAI: Составное свойство';
-$MESS['SMCP_MODULE_DESCRIPTION'] = 'Возможность использовать составное свойство для инфоблока';
-?>

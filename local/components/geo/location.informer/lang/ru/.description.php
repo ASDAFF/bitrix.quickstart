430199330b998d9bcfd4968b7a1932b9168680bc
ASDAFF <asdaff.asad@yandex.ru>
2019-07-26 19:54:40 +0500

    up

--- local/components/geo/location.informer/lang/ru/.description.php
+++ local/components/geo/location.informer/lang/ru/.description.php
@@ -1,4 +1,4 @@
 <?
 $MESS["GEO_LOCATION_INFORMER_COMPONENT_NAME"] = "Определение местоположения";
-$MESS["GEO_LOCATION_INFORMER_SECTION_NAME"] = "evalga.ru";
+$MESS["GEO_LOCATION_INFORMER_SECTION_NAME"] = "https://asdaff.github.io/";
 ?>

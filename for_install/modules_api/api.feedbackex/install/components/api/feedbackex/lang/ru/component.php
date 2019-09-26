e1b622b908a088830de1765ce56799732c376127
ASDAFF <asdaff.asad@yandex.ru>
2019-09-26 19:35:49 +0500

    up

--- for_install/modules_api/api.feedbackex/install/components/api/feedbackex/lang/ru/component.php
+++
@@ -1,5 +0,0 @@
-<?
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
-
-$MESS['API_FEX_MODULE_ERROR'] = 'Модуль "Расширенная форма обратной связи" не установлен';
--- for_install/modules_api/api.feedbackex/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.5.0",
-	"VERSION_DATE" => "2017-09-02 01:09:26"
-);
-?>

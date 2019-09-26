e97c2a53aec1c30544ca5d137cdc60b618955e53
ASDAFF <asdaff.asad@yandex.ru>
2019-09-26 19:23:25 +0500

    up

--- for_install/modules_api/api.feedback/install/_version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "2.3.0",
-	"VERSION_DATE" => "2015-05-25 23:53:09"
-);
-?>
--- for_install/modules_api/api.feedback/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "3.9.6",
-	"VERSION_DATE" => "2017-12-28 08:28:57"
-);
-?>

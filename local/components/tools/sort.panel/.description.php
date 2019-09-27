0482be9f57e17a4238b3fc88bcd8529deb8af838
ASDAFF <asdaff.asad@yandex.ru>
2019-09-27 10:35:58 +0500

    up

--- local/components/tools/sort.panel/.description.php
+++ local/components/tools/sort.panel/.description.php
@@ -10,14 +10,12 @@ $arComponentDescription = array(
 	"DESCRIPTION" => GetMessage("CODEBLOGPRO_SORT_PANEL_COMPONENT_DESCRIPTION_VALUE"),
 	"ICON" => "/images/icon.gif",
 	"SORT" => 100,
-	"PATH" => array(
-		"ID" => "ASDAFF",
-		"SORT" => 500,
-		"NAME" => GetMessage("CODEBLOGPRO_SORT.PANEL_COMPONENTS_FOLDER_NAME"),
-		"CHILD" => array(
-			"ID" => GetMessage("CODEBLOGPRO_SORT_PANEL_COMPONENT_TYPE_CONTENT_VALUE"),
-			"NAME" => '',
-			"SORT" => 500,
-		)
-	),
+    "PATH" => array(
+        "ID" => "ASDAFF",
+        "CHILD" => array(
+            "ID" => "content",
+            "NAME" => 'Контент',
+            "SORT" => 30
+        ),
+    ),
 );

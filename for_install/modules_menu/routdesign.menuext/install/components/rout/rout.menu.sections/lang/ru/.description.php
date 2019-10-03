46a36e0315fe78535074151e401740fd8e8c1f1f
ASDAFF <asdaff.asad@yandex.ru>
2019-10-03 18:47:07 +0500

    up

--- for_install/modules_menu/routdesign.menuext/install/components/rout/rout.menu.sections/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['T_IBLOCK_DESC_MENU_ITEMS'] = "Пункты меню (расширенные)";
-$MESS ['T_IBLOCK_DESC_MENU_ITEMS_DESC'] = "Дополнение меню названиями инфоблоков и секций";
-$MESS ['MAIN_NAVIGATION_SERVICE'] = "Навигация";
-?>

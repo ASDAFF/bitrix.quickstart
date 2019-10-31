282054f7a8286f702f159df3081196c568ba5226
ASDAFF <asdaff.asad@yandex.ru>
2019-11-01 00:45:30 +0500

    up

--- for_install/modules_geoip/nextype.geolocationreplacer/lang/ru/install/index.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS['NT_GEOLOCATION_REPLACER_INSTALL_NAME'] = 'Контент с привязкой к местоположению по IP';
-$MESS['NT_GEOLOCATION_REPLACER_MENU_NAME'] = 'Контент с привязкой к местоположению по IP';
-$MESS['NT_GEOLOCATION_REPLACER_INSTALL_DESCRIPTION'] = 'Модуль позволяет выводить различныый контент для местоположений определенных по IP с привязкой к городу или региону';
-$MESS['NT_GEOLOCATION_REPLACER_PARTNER'] = "Некстайп";
-$MESS['NT_GEOLOCATION_REPLACER_PARTNER_URI'] = "http://nextype.ru/";
-
-
-?>

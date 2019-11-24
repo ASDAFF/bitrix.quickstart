adfdc92fc8b1fc7d15af5a8b5dca7e63c9077310
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 22:17:28 +0500

    up

--- for_install/modules_interface/cube.ufpropertyhtml/lang/en/install/install.php
+++
@@ -1,9 +0,0 @@
-<?
-$MESS ['cubeufpropertyhtml_module_name'] = "Пользовательское свойcтво тип HTML";
-$MESS ['cubeufpropertyhtml_module_description'] = "Добавление пользовательских свойств тип HTML";
-$MESS ['cubeufpropertyhtml_install_title'] = "Установка модуля \"Пользовательское свойтво тип HTML\"";
-$MESS ['cubeufpropertyhtml_uninstall_title'] = "Деинсталляция модуля \"Пользовательское свойтво тип HTML\"";
-$MESS ['cubeufpropertyhtml_partner_name'] = "Адель Юсупов";
-$MESS ['cubeufpropertyhtml_include'] = 'Проверте в файле /bitrix/php_interface/init.php добавление строки require($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/uf_property_html/include.php")';
-$MESS ['cubeufpropertyhtml_uninclude'] = 'Удалите в файле /bitrix/php_interface/init.php строку require($_SERVER["DOCUMENT_ROOT"]."/bitrix/php_interface/uf_property_html/include.php")';
-?>

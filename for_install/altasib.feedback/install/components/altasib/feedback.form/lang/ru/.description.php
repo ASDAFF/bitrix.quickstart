70493afb601663a4172e65810d59a716f56d1766
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 23:14:07 +0500

    up

--- for_install/altasib.feedback/install/components/altasib/feedback.form/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS['ALX_DESC_FAQ_ADD_NAME'] = "Форма обратной связи";
-$MESS['ALX_DESC_REVIEW_ADD_DESC'] = "Форма обратной связи, позволяющая посетителям сайта отправлять сообщения владельцам ресурса";
-$MESS['ALX_DESC_FORMS_SECTION_NAME'] = "Формы";
-$MESS['ALX_DESC_SECTION_SECTION'] = 'АЛТАСИБ';
-?>

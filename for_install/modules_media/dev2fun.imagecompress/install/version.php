d62b938b889d4b535d8f8616109fcf053a687db4
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 23:02:04 +0500

    up

--- for_install/modules_media/dev2fun.imagecompress/install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-$arModuleVersion = array(
-    "VERSION" => "0.1.10",
-    "VERSION_DATE" => "2017-08-26 03:00:00",
-);
-?>
--- for_install/modules_media/dev2fun.imagecompress/lang/ru/include.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
-$MESS["DEV2FUN_IMAGECOMPRESS_MENU_TEXT"] = "Оптимизация картинок";
-$MESS["DEV2FUN_IMAGECOMPRESS_MENU_TITLE"] = "Оптимизация картинок";

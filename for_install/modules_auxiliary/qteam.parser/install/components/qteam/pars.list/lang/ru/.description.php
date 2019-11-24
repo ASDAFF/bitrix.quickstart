726d2a646c8e97e676eff1b7890a7f56832aa027
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 14:54:03 +0500

    up

--- for_install/modules_auxiliary/qteam.parser/install/components/qteam/pars.list/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["T_IBLOCK_DESC_LIST"] = "Парсер статей/новостей";
-$MESS["T_IBLOCK_DESC_LIST_DESC"] = "Список новостей из одного информационного блока.";
-$MESS["T_IBLOCK_DESC_NEWS"] = "Статьи и новости";
-$MESS["QTEAM_PARSER_PARSER_KONTENTA"] = "Парсер контента";
-?>

f54011162cbdd43307782d490a9af62e37fe0ce2
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 19:23:31 +0500

    up

--- for_install/modules_feedback/vasoft.likeit/lang/en/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$MESS['VASOFT_LIKEIT_MODULE_NAME'] = 'Likes for information block element';
-$MESS['VASOFT_LIKEIT_MODULE_DESCRIPTION'] = 'Likes for information block element';
-$MESS["VASOFT_LIKEIT_NEED_D7"] = "Need for D7 kernel";
-$MESS["VASOFT_LIKEIT_MODULE_REMOVING"] = "Uninstall likeit module";
--- for_install/modules_feedback/vasoft.likeit/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MESS['VASOFT_LIKEIT_MODULE_NAME'] = 'Лайки для элементов информационных блоков';
-$MESS['VASOFT_LIKEIT_MODULE_DESCRIPTION'] = 'Обработка лайков пользователей к элементам инфоблока';
-$MESS["VASOFT_LIKEIT_NEED_D7"] = "Требуется ядро D7";
-$MESS["VASOFT_LIKEIT_MODULE_REMOVING"] = "Удаление модуля лайков";
-$MESS["VASOFT_LIKEIT_NEED_IBLOCK"] = "Необходим модуль IBlock";

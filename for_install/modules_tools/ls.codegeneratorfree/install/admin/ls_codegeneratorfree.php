d23a86436ce790ba4b3c3da9e0d1e5897055b088
ASDAFF <asdaff.asad@yandex.ru>
2019-11-24 23:26:14 +0500

    up

--- for_install/modules_tools/ls.codegeneratorfree/install/admin/ls_codegeneratorfree.php
+++
@@ -1,1 +0,0 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/ls.codegeneratorfree/admin/ls_codegeneratorfree.php");?>
--- for_install/modules_tools/ls.codegeneratorfree/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.2.0",
-	"VERSION_DATE" => "2013-02-27 12:35:26"
-);
-?>

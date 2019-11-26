b80fae416c98354465b3a2afdb2b96e1b24284de
ASDAFF <asdaff.asad@yandex.ru>
2019-11-27 03:43:23 +0500

    up

--- for_install/modules_geoip/ss.geoip/install/bitrix/components/ss/geoip/templates/.default/lang/ru/template.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS['SS_GEOIP_LOADING'] = "Загрузка...";
-$MESS['SS_GEOIP_ENTER_CITY'] = "Укажите город";
-$MESS['SS_GEOIP_THIS_YOUR_CITY'] = "Вы в городе <span>#CITY#</span>?";
-$MESS['SS_GEOIP_POINT_YOUR_CITY'] = "Пожалуйста, укажите свой город.";
-$MESS['SS_GEOIP_PLACEHOLDER'] = "Город";
-?>

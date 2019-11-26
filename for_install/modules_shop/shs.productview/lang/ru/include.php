03b9f393cf5ce028cf6972bb24ab0ed89a9afd42
ASDAFF <asdaff.asad@yandex.ru>
2019-11-27 03:03:47 +0500

    up

--- for_install/modules_shop/shs.productview/lang/ru/include.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS['SHS_PEOPLE_STR_0'] = "человек";
-$MESS['SHS_PEOPLE_STR_1'] = "человека";
-$MESS['SHS_PEOPLE_STR_2'] = "человек";
-$MESS['SHS_DEMOEND_PROD'] = "<h6 style='color:red' >Срок <a target='_blank' href='http://marketplace.1c-bitrix.ru/solutions/shs.productview/'>демо версии</a>  истек! Функционал модуля будет недоступен</h6>";
-
-?>

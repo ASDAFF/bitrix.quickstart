5bf60470a5c97e46e0fd9b4b8faf4a516c37fded
ASDAFF <asdaff.asad@yandex.ru>
2019-11-27 14:14:43 +0500

    up

--- for_install/modules_shop/mlife.bistroklick/install/components/mlife.bistroclick/lang/ru/.description.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS ['MLIFE_CAT_BISTROCKLICK_NAME'] = "Заказ в 1 клик";
-$MESS ['MLIFE_CAT_BISTROCKLICK_DESCRIPTION'] = "Заказ в 1клик, с возможностью записи заказа в базу и уведомлений по смс";
-$MESS ['MLIFE'] = 'Mlife Media';
-$MESS ['MLIFE_ORDER'] = 'Корзина, заказы';
-?>
--- for_install/modules_shop/mlife.bistroklick/install/components/mlife.bistroclick/templates/.default/js/template.js
+++
@@ -1,2 +0,0 @@
-var $j = jQuery.noConflict();
-    $j('.photo_row .bg_mlife_photo_row a').fancybox({});

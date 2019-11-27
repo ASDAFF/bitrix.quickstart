e9e1d82376f583793b7345506c6c984e1b9d0edc
ASDAFF <asdaff.asad@yandex.ru>
2019-11-27 13:54:08 +0500

    up

--- for_install/modules_shop/iarga.cleverbasket/ajax/setcookie.php
+++
@@ -1,6 +0,0 @@
-<?include($_SERVER['DOCUMENT_ROOT']."/bitrix/modules/main/include/prolog_before.php");
-if(defined("BX_UTF") && BX_UTF=="Y"){
-	$_POST['name'] = iconv('utf-8','windows-1251',$_POST['name']);
-	$_POST['name'] = iconv('utf-8','windows-1251',$_POST['name']);
-}
-setcookie($_POST['name'],$_POST['val'],time()+24*3600*5,'/');?>
--- for_install/modules_shop/iarga.cleverbasket/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "0.1.5",
-	"VERSION_DATE" => "2014-02-14 13:00:00"
-);
-?>

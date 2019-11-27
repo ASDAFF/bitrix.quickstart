12952cd62754fe738b98de81e8708394edb41d81
ASDAFF <asdaff.asad@yandex.ru>
2019-11-27 13:49:10 +0500

    up

--- for_install/modules_shop/mk.rees46/install/components/rees46/recommend/component.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
-	die();
-}
-
-CModule::IncludeModule('mk.rees46');
-\Rees46\Component\RecommendHandler::run($arParams);
--- for_install/modules_shop/mk.rees46/lang/ru/install/step.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$MESS['REES_MODULE_INSTALLED'] = 'Модуль REES46 установлен';

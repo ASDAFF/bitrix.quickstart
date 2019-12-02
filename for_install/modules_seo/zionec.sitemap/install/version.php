6d7c6a3198e31c2ed6f159346b5a2a481942e05c
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 19:09:45 +0500

    up

--- for_install/modules_seo/zionec.sitemap/install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$arModuleVersion = array(
-    "VERSION" => "1.4.10",
-    "VERSION_DATE" => "2017-12-26 13:00:14"
-);
--- for_install/modules_seo/zionec.sitemap/lang/ru/admin/menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS["SITEMAP_MENU_ITEM"] = "Генератор Sitemap";
-$MESS["SITEMAP_MENU_TITLE"] = "Настройка генератора sitemap";
-?>

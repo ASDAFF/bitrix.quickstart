09dae0197972ab2f5f4df7404f4e688d21fddbbf
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 19:00:48 +0500

    up

--- for_install/modules_services/rksoft.selectcity/install/components/rksoft/select.city/lang/ru/.description.php
+++
@@ -1,8 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
-
-<?
-$MESS["SECTION_COMPONENT_NAME"] = "РК Софт";
-$MESS["SUB_SECTION_COMPONENT_NAME"] = "Интернет-магазин";
-$MESS["COMPONENT_NAME"] = "Выбор города покупателя";
-$MESS["COMPONENT_DESC"] = "Компонент определяет все группы местоположений и выводит в них входящие города и населенные пункты";
-?>
--- for_install/modules_services/rksoft.selectcity/install/components/rksoft/select.city/lang/ru/component.php
+++
@@ -1,5 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
-
-<?
-$MESS["rksoft.select_city_REQUIRED_MODULES_NOT_INSTALL"] = "Не все обязательные модули установлены в системе. Необходимы: iblock, catalog, sale";
-?>

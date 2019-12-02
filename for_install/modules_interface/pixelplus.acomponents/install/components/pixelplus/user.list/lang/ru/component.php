f91b25366bbd5d2e83bde6c7fa2dc8a1a98f0489
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 22:07:29 +0500

    up

--- for_install/modules_interface/pixelplus.acomponents/install/components/pixelplus/user.list/lang/ru/component.php
+++
@@ -1,7 +0,0 @@
-<?
-$MESS["CP_PXUL_USER_SEX_M"] = "Мужской";
-$MESS["CP_PXUL_USER_SEX_W"] = "Женский";
-$MESS["CP_PXUL_USER_YES"] = "Да";
-$MESS["CP_PXUL_USER_NO"] = "Нет";
-$MESS["CP_PXUL_USER_UPLOAD"] = "Скачать";
-?>
--- for_install/modules_interface/site05.usertypeyesno/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.0.0",
-	"VERSION_DATE" => "2015-11-11 10:02:51"
-);
-?>

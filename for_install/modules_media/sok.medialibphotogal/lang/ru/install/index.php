fb5fd8ca31ce9701b7d639f780fb33b625f98fc0
ASDAFF <asdaff.asad@yandex.ru>
2019-12-03 01:32:27 +0500

    up

--- for_install/modules_media/sok.medialibphotogal/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["sok.medialibphotogal_MODULE_NAME"] = "Галерея из медиабиблиотеки";
-$MESS["sok.medialibphotogal_MODULE_DESC"] = "Галерея из медиабиблиотеки";
-$MESS["sok.medialibphotogal_PARTNER_NAME"] = "ООО \"СОК\"";
-$MESS["sok.medialibphotogal_PARTNER_URI"] = "http://sok24.ru";
-?>

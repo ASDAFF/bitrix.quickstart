27c14216cd7dcfce411e5b855005995924b418eb
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 22:25:40 +0500

    up

--- for_install/modules_interface/mailtrig.events/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.2.0",
-	"VERSION_DATE" => "2014-06-04 20:35:33"
-);
-?>
--- for_install/modules_interface/mailtrig.events/lang/ru/admin/menu.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["MAILTRIG_EVENTS_MENU_MAIN"] = "MailTrig - триггерные рассылки";
-$MESS["MAILTRIG_EVENTS_MENU_MAIN_TITLE"] = "MailTrig - триггерные e-mail рассылки";
-$MESS["MAILTRIG_EVENTS_MENU_"] = "";
-?>

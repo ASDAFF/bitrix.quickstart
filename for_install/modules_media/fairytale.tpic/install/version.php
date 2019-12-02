bd025920a49475111c09a3c335b9d6bf124bf46a
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 19:53:36 +0500

    up

--- for_install/modules_media/fairytale.tpic/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.0.2",
-	"VERSION_DATE" => "2015-05-24 11:15:05"
-);
-?>
--- for_install/modules_media/zebrus.resize/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.2.1",
-	"VERSION_DATE" => "2013-02-22 12:57:33"
-);
-?>

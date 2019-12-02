ffede5890d64179d672c1c25d540f5b2692c2203
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 19:10:46 +0500

    up

--- for_install/modules_seo/nsandrey.sitemap/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS['NAS_SITEMAP_INSTALL_NAME'] = 'Расширенная карта сайта';
-$MESS['NAS_SITEMAP_INSTALL_DESCRIPTION'] = 'Позволяет добавить на страницу расширенную карту сайта для посетителей.';
-$MESS['NAS'] = 'Новиков Андрей Сергеевич';
-$MESS['NAS_URI'] = '';
-?>

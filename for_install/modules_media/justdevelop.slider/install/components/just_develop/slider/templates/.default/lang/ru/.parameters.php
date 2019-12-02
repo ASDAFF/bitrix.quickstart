84d3066a692d53cff50b0ab22a42a57633e3d684
ASDAFF <asdaff.asad@yandex.ru>
2019-12-03 03:02:04 +0500

    up

--- for_install/modules_media/justdevelop.slider/install/components/just_develop/slider/templates/.default/lang/ru/.parameters.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["IBLOCK_CB_TMPL_WIDTH"] = "Максимальная ширина изображения";
-$MESS["IBLOCK_CB_TMPL_HEIGHT"] = "Максимальная высота изображения";
-$MESS["IBLOCK_CB_TMPL_INTERVAL"] = "Интервал (сек.)";
-
-?>

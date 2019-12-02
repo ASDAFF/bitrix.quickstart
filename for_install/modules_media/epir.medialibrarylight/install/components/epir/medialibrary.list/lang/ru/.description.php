cc8bfe4d3ce67090cbb409b361fb8385a2b5b5cc
ASDAFF <asdaff.asad@yandex.ru>
2019-12-03 01:26:47 +0500

    up

--- for_install/modules_media/epir.medialibrarylight/install/components/epir/medialibrary.list/lang/ru/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS ['EPIR_COMPONENT_NAME'] = "Фотомедиатека";
-$MESS ['EPIR_COMPONENT_DESCRIPTION'] = "Отображение содержимого коллекций медиабиблиотеки";
-$MESS ['EPIR_IBLOCK_DESC'] = "Копоненты epir";
- ?>
--- for_install/modules_media/epir.medialibrarylight/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-	"VERSION" => "1.0.5",
-	"VERSION_DATE" => "2013-06-05 11:24:14"
-);
-?>

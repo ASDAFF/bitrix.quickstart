9d1b369d6b793d8804625054bf2fb5bd2519cc14
ASDAFF <asdaff.asad@yandex.ru>
2019-12-02 19:59:18 +0500

    up

---
+++ for_install/modules_media/justdevelop.slider/install/version.php
@@ -0,0 +1,6 @@
+<?
+$arModuleVersion = array(
+	"VERSION" => "1.0.0",
+	"VERSION_DATE" => "2014-06-09 10:31:11"
+);
+?>
---
+++ for_install/modules_media/justdevelop.slider/lang/ru/install/index.php
@@ -0,0 +1,21 @@
+<?
+$MESS["justdevelop.slider_MODULE_NAME"] = "Слайдер (Highload)";
+$MESS["justdevelop.slider_MODULE_DESC"] = "Просто слайдер";
+$MESS["justdevelop.slider_PARTNER_NAME"] = "Just Develop";
+$MESS["justdevelop.slider_PARTNER_URI"] = "http://justdevelop.pro";
+
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_FILE'] = 'Изображение';
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_LINK'] = 'Ссылка';
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_HEAD'] = 'Заголовок';
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_DESC'] = 'Описание';
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_BTNAME'] = 'Текст кнопки';
+$MESS['JD_CUSTOM_FIELD_NAME_RU_UF_SORT'] = 'Сортировка';
+
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_FILE'] = 'Image';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_LINK'] = 'Link';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_HEAD'] = 'eader';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_HEAD'] = 'Header';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_DESC'] = 'Description';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_BTNAME'] = 'Button text';
+$MESS['JD_CUSTOM_FIELD_NAME_EN_UF_SORT'] = 'Sort';
+?>

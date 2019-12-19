c8d34591575114f051090170d0f4f398858e5efe
ASDAFF <asdaff.asad@yandex.ru>
2019-12-19 15:16:41 +0500

    up

--- for_install/intellectservice.addbuyerprofile/lang/ru/install/index.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["intellectservice.addbuyerprofile_MODULE_NAME"] = "Добавление профиля покупателя";
-$MESS["intellectservice.addbuyerprofile_MODULE_DESC"] = "Модуль добавления профиля покупателя";
-$MESS["intellectservice.addbuyerprofile_PARTNER_NAME"] = "Интеллект-сервис";
-$MESS["intellectservice.addbuyerprofile_PARTNER_URI"] = "https://intellect-service.pro";
-?>
--- for_install/modules_altasib/altasib.feedback/install/version.php
+++
@@ -1,6 +0,0 @@
-<?
-$arModuleVersion = array(
-        "VERSION" => "2.7.0",
-        "VERSION_DATE" => "2017-07-10 10:00:00"
-);
-?>

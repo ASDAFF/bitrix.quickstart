90f25cbb0bda974efc1a1218be5b5931d45afc75
ASDAFF <asdaff.asad@yandex.ru>
2020-01-25 20:06:22 +0500

    clear

---
+++ for_install/2.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 25/1/2020 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: ASDAFF
+ * Date: 25.01.2020
+ * Time: 20:06
+ */

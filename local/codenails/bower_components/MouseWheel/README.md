978da0b9c511f06980cd3bc6639f56021cad6a0e
ASDAFF <asdaff.asad@yandex.ru>
2020-02-02 05:51:21 +0500

    up

--- local/codenails/bower_components/MouseWheel/README.md
+++
@@ -1,6 +0,0 @@
-MouseWheel.js
-=============
-
-A zero dependency ridiculous fast mousewheel handler.
-
-Compatible with: IE8+, FireFox, Chrome, Safari, Opera, iOS, Android
--- local/php_interface/config/const.php
+++ local/php_interface/config/const.php
@@ -21,13 +21,14 @@ define("IBLOCK_ID__VIDEO", 9);
 /**
  * PATHS
  */
-define('SITE_PATH_LOGO', '/local/codenails/frontend/images/logo');              //путь до логотипов сайта
-define('PATH_GLOBAL_CSS', '/local/codenails/frontend/css/global.css');
-define('PATH_GLOBAL_JS', '/local/codenails/frontend/js/global.js');             //путь до файлов со js скриптами
-define('PATH_RESPONSIVE_CSS', '/local/codenails/frontend/css/responsive.css');
+define('SITE_PATH_LOGO', '/local/assets/frontend/images/logo');                 //путь до логотипов сайта
+define('PATH_CSS', '/local/assets/frontend/css');                               //путь до файлов со стилями
+define('PATH_JS', '/local/assets/frontend/js');                                 //путь до файлов со js скриптами
+define('PATH_GLOBAL_CSS', '/local/assets/frontend/css/global.css');             //путь до файла со глобальными стилями
+define('PATH_GLOBAL_JS', '/local/assets/frontend/js/global.js');                //путь до файлов со js скриптами
+define('PATH_RESPONSIVE_CSS', '/local/assets/frontend/css/responsive.css');
+define('PATH_AJAX', '/local/ajax/');                                            //путь до директории ajax
 define('PATH_AJAX_JS', '/local/ajax/ajax.js');                                  //путь до директории ajax
-define('PATH_TEMPLATE_CSS', '/local/codenails/frontend/css');                   //путь до файлов со стилями
-define('PATH_TEMPLATE_JS', '/local/codenails/frontend/js');                     //путь до файлов со js скриптами
-define('PATH_BOWER_COMPONENTS', '/local/codenails/bower_components');           //путь до файлов библиотек
-define('PATH_LIBRARY', '/local/codenails/lib');                                 //путь до файлов библиотек
+define('PATH_BOWER_COMPONENTS', '/local/assets/bower_components');              //путь до файлов библиотек bower
+define('PATH_LIBRARY', '/local/assets/lib');                                    //путь до файлов библиотек
 define('PATH_INCLUDE', '/include/');

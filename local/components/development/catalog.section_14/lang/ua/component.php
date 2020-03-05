1e6037b2a1f8f20e31b1493d1b6ce9bb375efdd8
ASDAFF <asdaff.asad@yandex.ru>
2020-03-05 17:03:52 +0500

    up

---
+++ local/components/development/catalog.section_14/lang/ua/component.php
@@ -0,0 +1,9 @@
+<?
+$MESS["IBLOCK_MODULE_NOT_INSTALLED"] = "Модуль Інформаційних блоків не встановлено";
+$MESS["CATALOG_ERROR2BASKET"] = "Помилка додавання товару до кошику";
+$MESS["CATALOG_SECTION_NOT_FOUND"] = "Розділ не знайдений.";
+$MESS["CATALOG_PRODUCT_NOT_FOUND"] = "Товар не знайдено";
+$MESS["CATALOG_SUCCESSFUL_ADD_TO_BASKET"] = "Товар успішно доданий до кошика";
+$MESS["CATALOG_PARTIAL_BASKET_PROPERTIES_ERROR"] = "Не всі властивості товару, що додаються в корзину, заповнені";
+$MESS["CATALOG_EMPTY_BASKET_PROPERTIES_ERROR"] = "Чи не заповнені властивості товару, що додаються в кошик";
+?>

3cbdef81eb39daa184e9d75c5a1f1ed1a3facd93
ASDAFF <asdaff.asad@yandex.ru>
2020-05-26 01:54:51 +0500

    up

--- local/components/development/map.yandex.view/lang/ru/.description.php
+++ local/components/development/map.yandex.view/lang/ru/.description.php
@@ -3,8 +3,8 @@
  * Copyright (c) 25/7/2019 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
  */
 
-$MESS['MYMV_COMP_NAME'] = '������.�����: ������������� �����';
-$MESS['MYMV_COMP_DESCRIPTION'] = '������.����� � ������������ ������ ������������ ��������';
-$MESS['MAIN_YANDEX_MAP_SERVICE'] = '������.�����';
-$MESS['MAIN_Y_CONTENT'] = '�������';
+$MESS['MYMV_COMP_NAME'] = 'Yandex.Map';
+$MESS['MYMV_COMP_DESCRIPTION'] = 'Яндекс.Карта с настраиваемыми параметрами отображения.';
+$MESS['MAIN_YANDEX_MAP_SERVICE'] = 'Yandex.Map';
+$MESS['MAIN_Y_CONTENT'] = 'Контент';
 ?>

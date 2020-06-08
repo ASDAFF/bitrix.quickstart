55ad6dc975e1fafbcbcdc3445f982c137b1a7e2e
ASDAFF <asdaff.asad@yandex.ru>
2020-06-08 11:32:40 +0500

    up

--- local/components/development/simple.slider.gallery/lang/en/.description.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["SS_SSG_NAME"] = "Simple Photo Slider";
-$MESS["SS_SSG_DESCRIPTION"] = "Simple photo slider. Works from the \"Start\" edition";
-$MESS["SS_NAME"] = "Soft Service";
-?>
--- local/components/development/simple.slider.gallery/lang/ru/component.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS["IBLOCK_MODULE_NOT_INSTALED"] = "Модуль «Информационные блоки» не установлен!";
-$MESS["SSG_ELEMENTS_NA"] = "Элемент не найден.";
-?>

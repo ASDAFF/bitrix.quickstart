74fed67d8737d46ed8a05ddcc435476048221685
ASDAFF <asdaff.asad@yandex.ru>
2020-06-08 11:09:02 +0500

    up

--- local/components/development/inseco.slider/lang/ru/.description.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS ['INSECO_COMPONENT_NAME'] = "Компактный слайдер ";
-$MESS ['INSECO_COMPONENT_DESCRIPTION'] = "Компактный слайдер в виде табов с возможностью вставки медиа контента";
-?>

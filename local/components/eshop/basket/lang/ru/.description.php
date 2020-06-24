8dc6ee1bb099bab6ea30e66127266a05858df3c1
ASDAFF <asdaff.asad@yandex.ru>
2020-06-24 16:17:14 +0500

    up

--- local/components/eshop/basket/lang/ru/.description.php
+++
@@ -1,9 +0,0 @@
-<?
-/**
- * Copyright (c) 25/7/2019 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
- */
-
-$MESS ['BEONO_BASKET_NAME'] = "Корзина PRO";
-$MESS ['BEONO_BASKET_DESCRIPTION'] = "Выводит корзину текущего пользователя";
-$MESS ['BEONO_BASKET_CATNAME'] = "Корзина";
-?>

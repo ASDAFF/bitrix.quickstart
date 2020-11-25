d632c1cb3a02e80c087e556873d33f77c7f2bfa6
ASDAFF <asdaff.asad@yandex.ru>
2020-11-26 00:00:23 +0500

    fix composer

--- local/vendor/composer/autoload_static.php
+++ local/vendor/composer/autoload_static.php
@@ -44,12 +44,28 @@ class ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6
         0 => __DIR__ . '/../..' . '/php_interface/classes',
     );
 
+    public static $prefixesPsr0 = array (
+        'D' => 
+        array (
+            'Detection' => 
+            array (
+                0 => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/namespaced',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Mobile_Detect' => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/Mobile_Detect.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6::$prefixDirsPsr4;
             $loader->fallbackDirsPsr4 = ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6::$fallbackDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit0330bb43122e8e9a158da54d2d3713d6::$classMap;
 
         }, null, ClassLoader::class);
     }

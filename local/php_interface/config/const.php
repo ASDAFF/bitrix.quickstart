a6ed6d0fe772446b7b4bbb0103c23fea9a0beeb2
ASDAFF <asdaff.asad@yandex.ru>
2022-05-31 13:11:52 +0500

    edit path

--- local/php_interface/config/const.php
+++ local/php_interface/config/const.php
@@ -21,12 +21,12 @@ define("IBLOCK_ID__VIDEO", 9);
 /**
  * PATHS
  */
-define('SITE_PATH_LOGO', '/local/assets/frontend/images/logo');                 //путь до логотипов сайта
-define('PATH_CSS', '/local/assets/frontend/css');                               //путь до файлов со стилями
-define('PATH_JS', '/local/assets/frontend/js');                                 //путь до файлов со js скриптами
-define('PATH_GLOBAL_CSS', '/local/assets/frontend/css/global.css');             //путь до файла со глобальными стилями
-define('PATH_GLOBAL_JS', '/local/assets/frontend/js/global.js');                //путь до файлов со js скриптами
-define('PATH_RESPONSIVE_CSS', '/local/assets/frontend/css/responsive.css');
+define('SITE_PATH_LOGO', '/local/assets/images/logo');                 //путь до логотипов сайта
+define('PATH_CSS', '/local/assets/css');                               //путь до файлов со стилями
+define('PATH_JS', '/local/assets/js');                                 //путь до файлов со js скриптами
+define('PATH_GLOBAL_CSS', '/local/assets/css/global.css');             //путь до файла со глобальными стилями
+define('PATH_GLOBAL_JS', '/local/assets/js/global.js');                //путь до файлов со js скриптами
+define('PATH_RESPONSIVE_CSS', '/local/assets/css/responsive.css');
 define('PATH_AJAX', '/local/ajax/');                                            //путь до директории ajax
 define('PATH_AJAX_JS', '/local/ajax/ajax.js');                                  //путь до директории ajax
 define('PATH_BOWER_COMPONENTS', '/local/assets/bower_components');              //путь до файлов библиотек bower
